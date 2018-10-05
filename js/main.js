40f9b2202def3a11ef0c09eeeefcb51f5bf322f1
Michał Burzyński <michal.b89@wp.pl>
2018-10-06 01:15:33 +0200

    Added output variable containing dynamic HTML which is inserted into empty div container every time the user submits form.

--- js/main.js
+++ js/main.js
@@ -26,6 +26,22 @@ document.getElementById('searchInputForm').addEventListener('submit', function (
             const dataList = this.response.results;
             dataList.forEach(listItem => {
                 const character = new Character(listItem.name, listItem.height, listItem.mass, listItem.hair_color, listItem.skin_color, listItem.eye_color, listItem.birth_year, listItem.gender);
+
+                output += `<div class="col-md-6 mb-3">
+                                <div class="card bg-warning text-uppercase font-weight-bold">
+                                    <div class="card-header py-3">Name: ${character.name}</div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Height: ${character.height}</li>
+                                        <li class="list-group-item">Mass: ${character.mass}</li>
+                                        <li class="list-group-item">Hair color: ${character.hairColor}</li>
+                                        <li class="list-group-item">Skin color: ${character.skinColor}</li>
+                                        <li class="list-group-item">Eye color: ${character.eyeColor}</li>
+                                        <li class="list-group-item">Birth year: ${character.birthYear}</li>
+                                        <li class="list-group-item">Gender: ${character.gender}</li>
+                                    </ul>
+                                </div>
+                            </div>`;
+                document.querySelector('.col-lg-8 .row').innerHTML = output;
             });
         } else {
             console.log('The connection ended with status ' + this.statusText);
